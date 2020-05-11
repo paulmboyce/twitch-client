ffa000200fdf23da591aeffe42284c49cd5b3999
paulmboyce <paul.boyce@goobito.com>
2020-05-11 11:54:49 -0300

    Refactored to tighten up code. Move exlicit bind(this) to arrow functions

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,56 +1,38 @@
 import React from "react";
 
-class GoogleAuth extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			auth2: null,
-			authLoaded: null,
-			buttonText: "",
-			clickAction: () => {
-				console.log("Waiting for status..");
-			},
-		};
+const GOOGLE_CLIENT_ID =
+	"931872608637-0n3q7ru6jisa5g43rofhtkru2daou7gq.apps.googleusercontent.com";
 
-		window.gapi.load(
-			"auth2",
-			function () {
-				const auth2 = window.gapi.auth2.init({
-					clientId:
-						"931872608637-0n3q7ru6jisa5g43rofhtkru2daou7gq.apps.googleusercontent.com",
-					scope: "email",
-				});
-				auth2.then(() => {
-					this.setState({ auth2: auth2, authLoaded: true });
-					console.log("GoogleAuth loaded..OK");
-				});
-			}.bind(this)
-		);
-	}
+class GoogleAuth extends React.Component {
+	state = {
+		auth2: null,
+		authLoaded: null,
+		buttonText: "",
+		clickAction: () => {},
+	};
 
 	componentDidMount = () => {
-		if (!this.state.authLoaded) {
-			console.log("Waiting for auth2...");
-			setTimeout(this.componentDidMount, 300);
-		} else {
-			this.updateLoginButton();
-		}
+		window.gapi.load("auth2", () => {
+			window.gapi.auth2
+				.init({ clientId: GOOGLE_CLIENT_ID })
+				.then((auth2) => {
+					console.log("GoogleAuth loaded..OK");
+					this.setState({ auth2: auth2, authLoaded: true });
+					this.updateLoginButton();
+				});
+		});
 	};
 
 	logIn = () => {
-		this.state.auth2
-			.signIn({
-				scope: "profile email",
-			})
-			.then(
-				function success(user) {
-					console.log("Logged in OK, user: ", user);
-					this.updateLoginButton();
-				}.bind(this),
-				function rejected(err) {
-					console.log("HOUSTON.. we have an: ", err);
-				}
-			);
+		this.state.auth2.signIn({ scope: "profile email" }).then(
+			(user) => {
+				console.log("Logged in OK, user: ", user);
+				this.updateLoginButton();
+			},
+			function rejected(err) {
+				console.log("HOUSTON.. we have an: ", err);
+			}
+		);
 	};
 
 	logOut = () => {
@@ -62,17 +44,14 @@ class GoogleAuth extends React.Component {
 
 	updateLoginButton = function () {
 		const signedIn = this.state.auth2.isSignedIn.get();
-		console.log(
-			`User ${
-				signedIn ? `IS` : `is NOT`
-			} signed in. Setting Login/Out Button....`
-		);
-
 		if (signedIn) {
-			this.setState({ buttonText: "Log Out", clickAction: this.logOut });
+			this.setState({
+				buttonText: "Log Out",
+				clickAction: this.logOut,
+			});
 		} else {
 			this.setState({
-				buttonText: "Log In With Google",
+				buttonText: "Log in with Google",
 				clickAction: this.logIn,
 			});
 		}
