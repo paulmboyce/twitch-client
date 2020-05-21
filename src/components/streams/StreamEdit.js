4fa445b3f189824ac78fc8a970aa3d7f167c196a
paulmboyce <paul.boyce@goobito.com>
2020-05-21 11:17:20 -0300

    FEATURE: Get stream from state. Works on Link because streams are loaded. Fails on direct beecause streams not loaded.

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
 
 const StreamEdit = (props) => {
+	const { stream } = props;
 	console.log("PROPS: ", props);
-	return <div>StreamEdit STREAM: {props.match.params.id}</div>;
+	return (
+		<div>
+			StreamEdit STREAM: {props.match.params.id}
+			<p>TITLE: {stream ? stream.title : null}</p>
+			<p>DESC: {stream ? stream.description : null}</p>
+		</div>
+	);
 };
 
-export { StreamEdit };
+const mapStateToProps = function (state, oldProps) {
+	console.log("STATE:", state);
+	console.log("OLD PROPS: ", oldProps);
+	return { stream: state.streams[oldProps.match.params.id] };
+};
+
+const connected = connect(mapStateToProps)(StreamEdit);
+export { connected as StreamEdit };
