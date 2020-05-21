d880b97cee7325b37ff35a8c64a953295822832b
paulmboyce <paul.boyce@goobito.com>
2020-05-21 10:58:17 -0300

    Get stream id from URL via props.match.params (similar to ExpressJS)

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-const StreamEdit = () => {
-	return <div>StreamEdit</div>;
+const StreamEdit = (props) => {
+	console.log("PROPS: ", props);
+	return <div>StreamEdit STREAM: {props.match.params.id}</div>;
 };
 
 export { StreamEdit };
