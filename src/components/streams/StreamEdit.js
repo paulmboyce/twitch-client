86cc7bf95404309115a5d21e58bd4383d5814055
paulmboyce <paul.boyce@goobito.com>
2020-05-21 11:24:59 -0300

    FIX: If no streams, use ActionCreator to get this one strem from server

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const StreamEdit = (props) => {
-	const { stream } = props;
-	console.log("PROPS: ", props);
-	return (
-		<div>
-			StreamEdit STREAM: {props.match.params.id}
-			<p>TITLE: {stream ? stream.title : null}</p>
-			<p>DESC: {stream ? stream.description : null}</p>
-		</div>
-	);
-};
+import { getStream } from "../../actioncreators";
+
+class StreamEdit extends React.Component {
+	componentDidMount = () => {
+		const { stream, getStream, match } = this.props;
+
+		if (!stream) {
+			getStream(match.params.id);
+		}
+	};
+
+	render() {
+		const { stream } = this.props;
+		console.log("PROPS: ", this.props);
+		return (
+			<div>
+				StreamEdit STREAM: {this.props.match.params.id}
+				<p>TITLE: {stream ? stream.title : null}</p>
+				<p>DESC: {stream ? stream.description : null}</p>
+			</div>
+		);
+	}
+}
 
 const mapStateToProps = function (state, oldProps) {
 	console.log("STATE:", state);
@@ -19,5 +31,5 @@ const mapStateToProps = function (state, oldProps) {
 	return { stream: state.streams[oldProps.match.params.id] };
 };
 
-const connected = connect(mapStateToProps)(StreamEdit);
+const connected = connect(mapStateToProps, { getStream })(StreamEdit);
 export { connected as StreamEdit };
